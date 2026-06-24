4728cafb1dc182d5f625fd17a31f7e719029b5aa
erem1t0 <vladislav.nevsky.05@mail.ru>
2026-06-24 23:57:43 +0300

    Added documentation

--- include/OperationApplier.hpp
+++ include/OperationApplier.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file OperationApplier.hpp
+ * @brief Defines the apply_op function template for dispatching framework operations to container wrappers.
+ */
+
 #pragma once
 
 #include <type_traits>
@@ -8,12 +13,32 @@
 
 namespace valid_framework { 
     
+    /**
+     * @brief Applies a framework operation to a container through a wrapper.
+     * 
+     * Dispatches @p op to the corresponding static method of @p Wrapper:
+     * @c Wrapper::insert, @c Wrapper::get, @c Wrapper::erase, or @c Wrapper::custom.
+     * 
+     * @tparam Wrapper Adapter type that satisfies ContainerWrapper.
+     * @tparam Container Container type adapted by @c Wrapper.
+     * @tparam Key Operation key type.
+     * @tparam Value Operation value type.
+     * 
+     * @param[in,out] container Container instance to which the operation is applied.
+     * @param[in] op Operation to be applied.
+     * 
+     * @return An OperationResult containing the result of the applied @p op.
+     * 
+     * @see ContainerWrapper
+     * @see Operation
+     * @see OperationResult
+     */
     template<typename Wrapper, typename Container, typename Key, typename Value>
     requires ContainerWrapper<Wrapper, Container, Key, Value>
     OperationResult<Key, Value> apply_op(Container& container, const Operation<Key, Value>& op) {
         return std::visit(
             [&](const auto& x) -> OperationResult<Key, Value> {
-                using T = std:: decay_t<decltype(x)>;
+                using T = std::decay_t<decltype(x)>;
 
                 if constexpr(std::is_same_v<T, InsertOp<Key, Value>>) {
                     return BoolResult{Wrapper::insert(container,x.key, x.value)};
